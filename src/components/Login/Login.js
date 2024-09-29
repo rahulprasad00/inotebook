c25130ddd7efea6d679b4c1b322e90396fcb77e2
Adarsh208501 <adarshsharmadbg01@gmail.com>
2024-09-29 19:06:53 +0530

    guest login added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,6 +7,7 @@ const Login = (props) => {
 
     const host = "http://localhost:5000/";
     const [credentials, setcredentials] = useState({ email: "", password: "" });
+    const [isGuest, setisGuest] = useState(false)
 
     let navigate=useNavigate();
     const handleSubmit = async (e) => {
@@ -37,6 +38,10 @@ const Login = (props) => {
     const onChange = (e) => {
         setcredentials({ ...credentials, [e.target.name]: e.target.value })
     }
+    const handleGuestClick = () => {
+        setcredentials({ email:"guest@example.com" , password:"12345" })
+    }
+    
     return (
         <div className="flex flex-wrap">
             <div className="flex w-full flex-col md:w-1/2">
@@ -58,7 +63,8 @@ const Login = (props) => {
                                 <input type="password" id="password" name='password' className="w-full flex-1 appearance-none border-gray-300 bg-white px-4 py-2 text-base text-gray-700 placeholder-gray-400 focus:outline-none" placeholder="Password" value={credentials.password} onChange={onChange} required minLength={5}/>
                             </div>
                         </div>
-                        <button type="submit" className="w-full rounded-lg bg-gray-900 px-4 py-2 text-center text-base font-semibold text-white shadow-md ring-gray-500 ring-offset-2 transition focus:ring-2" >Log in</button>
+                        <button type="submit" className="w-full rounded-lg bg-gray-900 px-4 py-2 my-2 text-center text-base font-semibold text-white shadow-md ring-gray-500 ring-offset-2 transition focus:ring-2" >Log in</button>
+                        <button type="submit" className="w-full rounded-lg bg-gray-200 px-4 py-2 text-center text-base font-semibold text-black shadow-md ring-gray-500 ring-offset-2 transition focus:ring-2" onClick={handleGuestClick}>Guest Login</button>
                     </form>
                     <div className="py-12 text-center">
                         <p className="whitespace-nowrap text-gray-600">
